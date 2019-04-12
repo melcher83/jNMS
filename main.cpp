249cbe1b86e654efa58ca46b5b8fd30d500e8f6a
JonathanMelcher <melcher83@gmail.com>
2019-04-12 09:45:32 -0500

    Changed SNMP library

--- main.cpp
+++ main.cpp
@@ -1,74 +1,172 @@
-/*_############################################################################
-  _## 
-  _##  test_app.cpp  
-  _##
-  _##  SNMP++ v3.3
-  _##  -----------------------------------------------
-  _##  Copyright (c) 2001-2013 Jochen Katz, Frank Fock
-  _##
-  _##  This software is based on SNMP++2.6 from Hewlett Packard:
-  _##  
-  _##    Copyright (c) 1996
-  _##    Hewlett-Packard Company
-  _##  
-  _##  ATTENTION: USE OF THIS SOFTWARE IS SUBJECT TO THE FOLLOWING TERMS.
-  _##  Permission to use, copy, modify, distribute and/or sell this software 
-  _##  and/or its documentation is hereby granted without fee. User agrees 
-  _##  to display the above copyright notice and this license notice in all 
-  _##  copies of the software and any documentation of the software. User 
-  _##  agrees to assume all liability for the use of the software; 
-  _##  Hewlett-Packard and Jochen Katz make no representations about the 
-  _##  suitability of this software for any purpose. It is provided 
-  _##  "AS-IS" without warranty of any kind, either express or implied. User 
-  _##  hereby grants a royalty-free license to any and all derivatives based
-  _##  upon this software code base. 
-  _##  
-  _##########################################################################*/
-char test_app_cpp_version[]="@(#) SNMP++ $Id$";
-#include <libsnmp.h>
-#include<iostream>
+#include "net-snmp-config.h"
+#include "net-snmp-includes.h"
+#include <string.h>
 
-#include <snmp_pp.h>
+/* change the word "define" to "undef" to try the (insecure) SNMPv1 version */
+#define DEMO_USE_SNMP_VERSION_3
 
-#ifdef WIN32
-#define strcasecmp _stricmp
+#ifdef DEMO_USE_SNMP_VERSION_3
+const char *our_v3_passphrase = "The Net-SNMP Demo Password";
 #endif
 
-#ifdef SNMP_PP_NAMESPACE
-using namespace Snmp_pp;
-#endif
-
-// default request oids
-#define NUM_SYS_VBS	6
-#define sysDescr	"1.3.6.1.2.1.1.1.0"
-#define sysObjectID	"1.3.6.1.2.1.1.2.0"
-#define sysUpTime	"1.3.6.1.2.1.1.3.0"
-#define sysContact	"1.3.6.1.2.1.1.4.0"
-#define sysName		"1.3.6.1.2.1.1.5.0"
-#define sysLocation	"1.3.6.1.2.1.1.6.0"
-//#define sysServices	"1.3.6.1.2.1.1.7.0" // not all agents support this...
-
-// default notification oid
-#define coldStart	"1.3.6.1.6.3.1.1.4.3.0.1"
-
-int main(int argc, char **argv)
+int main(int argc, char ** argv)
 {
-    std::cout<<"Hello WORLD!";
+    netsnmp_session session, *ss;
+    netsnmp_pdu *pdu;
+    netsnmp_pdu *response;
 
+    oid anOID[MAX_OID_LEN];
+    size_t anOID_len;
+
+    netsnmp_variable_list *vars;
     int status;
-    char *req_str      = (char*) "get";
-    //  char *dflt_req_oid = (char*) sysDescr;
-    char *dflt_trp_oid = (char*) coldStart;
-    char *genAddrStr   = (char*) "127.0.0.1" ;		  // localhost
-    char *oid_str      = (char*) NULL;
+    int count=1;
 
-    if (argc > 1) genAddrStr = argv[1];
-    if (argc > 2) req_str    = argv[2];
-    if (argc > 3) oid_str    = argv[3];
+    /*
+     * Initialize the SNMP library
+     */
+    init_snmp("snmpdemoapp");
 
-    Snmp::socket_startup();  // Initialize socket subsystem
+    /*
+     * Initialize a "session" that defines who we're going to talk to
+     */
+    snmp_sess_init( &session );                   /* set up defaults */
+    session.peername = strdup("test.net-snmp.org");
 
-    IpAddress ipAddr(genAddrStr);
+    /* set up the authentication parameters for talking to the server */
 
-    return(0);
-}
+#ifdef DEMO_USE_SNMP_VERSION_3
+
+    /* Use SNMPv3 to talk to the experimental server */
+
+    /* set the SNMP version number */
+    session.version=SNMP_VERSION_3;
+
+    /* set the SNMPv3 user name */
+    session.securityName = strdup("MD5User");
+    session.securityNameLen = strlen(session.securityName);
+
+    /* set the security level to authenticated, but not encrypted */
+    session.securityLevel = SNMP_SEC_LEVEL_AUTHNOPRIV;
+
+    /* set the authentication method to MD5 */
+    session.securityAuthProto = usmHMACMD5AuthProtocol;
+    session.securityAuthProtoLen = sizeof(usmHMACMD5AuthProtocol)/sizeof(oid);
+    session.securityAuthKeyLen = USM_AUTH_KU_LEN;
+
+    /* set the authentication key to a MD5 hashed version of our
+       passphrase "The Net-SNMP Demo Password" (which must be at least 8
+       characters long) */
+    if (generate_Ku(session.securityAuthProto,
+                    session.securityAuthProtoLen,
+                    (u_char *) our_v3_passphrase, strlen(our_v3_passphrase),
+                    session.securityAuthKey,
+                    &session.securityAuthKeyLen) != SNMPERR_SUCCESS) {
+        snmp_perror(argv[0]);
+        snmp_log(LOG_ERR,
+                 "Error generating Ku from authentication pass phrase. \n");
+        exit(1);
+    }
+
+#else /* we'll use the insecure (but simplier) SNMPv1 */
+
+    /* set the SNMP version number */
+    session.version = SNMP_VERSION_1;
+
+    /* set the SNMPv1 community name used for authentication */
+    session.community = "demopublic";
+    session.community_len = strlen(session.community);
+
+#endif /* SNMPv1 */
+
+    /*
+     * Open the session
+     */
+    SOCK_STARTUP;
+    ss = snmp_open(&session);                     /* establish the session */
+
+    if (!ss) {
+        snmp_sess_perror("ack", &session);
+        SOCK_CLEANUP;
+        exit(1);
+    }
+
+    /*
+     * Create the PDU for the data for our request.
+     *   1) We're going to GET the system.sysDescr.0 node.
+     */
+    pdu = snmp_pdu_create(SNMP_MSG_GET);
+    anOID_len = MAX_OID_LEN;
+    if (!snmp_parse_oid(".1.3.6.1.2.1.1.1.0", anOID, &anOID_len)) {
+        snmp_perror(".1.3.6.1.2.1.1.1.0");
+        SOCK_CLEANUP;
+        exit(1);
+    }
+#if OTHER_METHODS
+    /*
+     *  These are alternatives to the 'snmp_parse_oid' call above,
+     *    e.g. specifying the OID by name rather than numerically.
+     */
+    read_objid(".1.3.6.1.2.1.1.1.0", anOID, &anOID_len);
+    get_node("sysDescr.0", anOID, &anOID_len);
+    read_objid("system.sysDescr.0", anOID, &anOID_len);
+#endif
+
+    snmp_add_null_var(pdu, anOID, anOID_len);
+
+    /*
+     * Send the Request out.
+     */
+    status = snmp_synch_response(ss, pdu, &response);
+
+    /*
+     * Process the response.
+     */
+    if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR) {
+        /*
+         * SUCCESS: Print the result variables
+         */
+
+        for(vars = response->variables; vars; vars = vars->next_variable)
+            print_variable(vars->name, vars->name_length, vars);
+
+        /* manipuate the information ourselves */
+        for(vars = response->variables; vars; vars = vars->next_variable) {
+            if (vars->type == ASN_OCTET_STR) {
+                char *sp = (char *)malloc(1 + vars->val_len);
+                memcpy(sp, vars->val.string, vars->val_len);
+                sp[vars->val_len] = '\0';
+                printf("value #%d is a string: %s\n", count++, sp);
+                free(sp);
+            }
+            else
+                printf("value #%d is NOT a string! Ack!\n", count++);
+        }
+    } else {
+        /*
+         * FAILURE: print what went wrong!
+         */
+
+        if (status == STAT_SUCCESS)
+            fprintf(stderr, "Error in packet\nReason: %s\n",
+                    snmp_errstring(response->errstat));
+        else if (status == STAT_TIMEOUT)
+            fprintf(stderr, "Timeout: No response from %s.\n",
+                    session.peername);
+        else
+            snmp_sess_perror("snmpdemoapp", ss);
+
+    }
+
+    /*
+     * Clean up:
+     *  1) free the response.
+     *  2) close the session.
+     */
+    if (response)
+        snmp_free_pdu(response);
+    snmp_close(ss);
+
+    SOCK_CLEANUP;
+    return (0);
+} /* main() */
